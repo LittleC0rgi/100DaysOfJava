63db10aa0dcf850a642357809525091f1e57e847
Kirill Vasilev <kirillwork56@gmail.com>
2025-12-29 22:25:12 +0300

    update log

---
+++ files/App.java
@@ -0,0 +1,15 @@
+class App {
+    public static void main(String[] args) {
+        var family = "Targaryen";
+
+        // BEGIN (write your solution here)
+        var countOfMyBrothers = 2;
+        System.out.println(countOfMyBrothers);
+        // END
+
+        System.out.print(family);
+        System.out.print(" and ");
+        System.out.print(pet);
+    }
+
+}
