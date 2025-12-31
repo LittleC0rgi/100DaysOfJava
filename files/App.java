52ac6835b75493c6a3a4f8d1506aab2d5e1ab09f
Kirill Vasilev <kirillwork56@gmail.com>
2025-12-31 08:09:36 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -2,10 +2,8 @@ class App {
     public static void main(String[] args) {
 
         // BEGIN (write your solution here)
-        var firstNumber = 11;
-        var secondNumber = -100;
-        var sum = firstNumber + secondNumber;
-        System.out.println(sum);
+
+
         // END
     }
 }
