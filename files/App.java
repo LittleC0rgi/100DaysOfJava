f74175619d9bc054c23230efea2ef56a98046898
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-01 23:43:17 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,10 +1,11 @@
-
 public class App {
     public static void main(String[] args) {
+        var one = "Naharis";
+        var two = "Mormont";
+        var three = "Sand";
+
         // BEGIN (write your solution here)
-        var num = (int) 5.1;
-        String str = num + " times";
-        System.out.println(str);
+        System.out.println(one.charAt(2) + " " + two.charAt(1) + " " + three.charAt(3) + " " + two.charAt(4) + " " + two.charAt(2) );
         // END
     }
 }
