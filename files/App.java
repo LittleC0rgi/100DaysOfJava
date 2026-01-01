43117ae0c992fb6b12338e8a1080ddc61bc05a46
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-01 22:26:51 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,9 +1,10 @@
-class App {
+
+public class App {
     public static void main(String[] args) {
-
         // BEGIN (write your solution here)
-
-
+        var num = (int) 5.1;
+        String str = num + " times";
+        System.out.println(str);
         // END
     }
 }
