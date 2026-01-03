2b46777704133e75f5b2e54556ed42c52075046c
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-03 22:29:49 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,12 +1,9 @@
+
 public class App {
     public static void main(String[] args) {
-       var name = "heXlEt";
-
-        // BEGIN (write your solution here
-        var result =
-    name.substring(0, 1).toUpperCase() +
-    name.substring(1).toLowerCase();
-        System.out.println(result);
+        // BEGIN (write your solution here)
+        var emoji = "-(";
+        System.out.println(":" + emoji.replace("(", ")"));
         // END
     }
 }
