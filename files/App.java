b124776dec3a9d14827067156d7771782dd38f9d
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-04 22:57:28 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,9 +1,9 @@
-
 public class App {
     public static void main(String[] args) {
-        // BEGIN (write your solution here)
-        var emoji = "-(";
-        System.out.println(":" + emoji.replace("(", ")"));
-        // END
+        App.gogo();
+    }
+
+    static void gogo() {
+        System.out.println("It works!");
     }
 }
