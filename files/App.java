0e6da0f31f1e87d692d933be9af1de09a6ccea35
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-05 20:36:57 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,25 +1,10 @@
-class App {
-    public static String getHiddenCard(String cardNumber, int countDot) {
-        return "*".repeat(countDot) + cardNumber.substring(12);
-    }
 
-    public static String getHiddenCard(String cardNumber) {
-        return App.getHiddenCard(cardNumber, 4);
+class App {
+    public static boolean isInternationalPhone(String phone) {
+        return phone.startsWith("+");
     }
 
     public static void main(String[] args) {
-        System.out.println(
-                App.getHiddenCard("2034399002121100", 1) // "*1100"
-        );
 
-        System.out.println(
-                App.getHiddenCard("1234567812345678", 2) // "**5678"
-
-        );
-
-        System.out.println(
-                App.getHiddenCard("1234567812345678", 3) // "***5678"
-
-        );
     }
 }
