7d3d3f70a82265493baad7c916713d9c3964e17d
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-05 19:41:41 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,5 +1,25 @@
 class App {
-    public static String sayHurrayThreeTimes() {
-        return "hurray! hurray! hurray!";
+    public static String getHiddenCard(String cardNumber, int countDot) {
+        return "*".repeat(countDot) + cardNumber.substring(12);
+    }
+
+    public static String getHiddenCard(String cardNumber) {
+        return App.getHiddenCard(cardNumber, 4);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                App.getHiddenCard("2034399002121100", 1) // "*1100"
+        );
+
+        System.out.println(
+                App.getHiddenCard("1234567812345678", 2) // "**5678"
+
+        );
+
+        System.out.println(
+                App.getHiddenCard("1234567812345678", 3) // "***5678"
+
+        );
     }
 }
