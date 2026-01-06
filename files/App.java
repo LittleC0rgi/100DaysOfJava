6c22d710c609aa1098760fd92faa8695eb68c1dd
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-06 08:03:16 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,10 +1,6 @@
 
 class App {
-    public static boolean isInternationalPhone(String phone) {
-        return phone.startsWith("+");
-    }
 
     public static void main(String[] args) {
-
     }
 }
