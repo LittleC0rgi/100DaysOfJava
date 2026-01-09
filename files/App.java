74d5e04c2cea7cf41e77306b1d0b8eab5c7654eb
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-09 23:50:51 +0300

    Test commit in zed

--- files/App.java
+++ files/App.java
@@ -1,36 +1,6 @@
-
-class App {
-    public static String encrypt(String str) {
-        // BEGIN (write your solution here)
-        String res = "";
-        String stock = null;
-        for (int i = 0; i < str.length(); i++) {
-            if (stock == null) {
-                stock = Character.toString(str.charAt(i));
-            } else {
-                res += str.charAt(i);
-                res += stock;
-                stock = null;
-            }
-        }
-
-        if (stock != null) {
-            res += stock;
-        }
-
-        return res;
-        // END
-    }
+public class App {
 
     public static void main(String[] args) {
-        System.out.println(
-                encrypt("move") // "omev"
-        );
-        System.out.println(
-                encrypt("attack") // "taatkc"
-        );
-        System.out.println(
-                encrypt("go!") // "og!"
-        );
+        System.out.println("Test comp");
     }
 }
