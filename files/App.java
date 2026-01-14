cac8681558e845d922fdcf4b10cb535fd74544a2
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-14 18:47:12 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,19 +1,40 @@
+import java.util.Arrays;
+
 class App {
-  public static int mult(int[] numbers) {
+  public static int[] getSameParity(int[] numbers) {
     if (numbers.length == 0) {
-      return 0;
+      return numbers;
     }
 
-    int sum = 1;
-    for (int num : numbers) {
-      sum *= num;
+    int firstNumberParity = Math.abs(numbers[0]) % 2;
+    int[] tempArrNumbers = new int[numbers.length];
+    int count = 0;
+
+    for (int number : numbers) {
+      if (Math.abs(number) % 2 == firstNumberParity) {
+        tempArrNumbers[count] = number;
+        count++;
+      }
     }
-    return sum;
+
+    return Arrays.copyOfRange(tempArrNumbers, 0, count);
   }
 
   public static void main(String[] args) {
-    int[] numbers1 = { 1, 4, 3, 4, 5 };
-    var res = App.mult(numbers1); // 240
-    System.err.println(res);
+    // Тестовые примеры
+    int[] numbers1 = {};
+    System.out.println(Arrays.toString(getSameParity(numbers1))); // []
+
+    int[] numbers2 = { 1, 2, 3 };
+    System.out.println(Arrays.toString(getSameParity(numbers2))); // [1, 3]
+
+    int[] numbers3 = { 1, 2, 8 };
+    System.out.println(Arrays.toString(getSameParity(numbers3))); // [1]
+
+    int[] numbers4 = { 2, 2, 8 };
+    System.out.println(Arrays.toString(getSameParity(numbers4))); // [2, 2, 8]
+
+    int[] numbers5 = { -3, 2, 1 };
+    System.out.println(Arrays.toString(getSameParity(numbers5))); // [-3, 1]
   }
 }
