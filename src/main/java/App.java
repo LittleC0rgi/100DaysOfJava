6612ec355b34f64a6722d1f69de1c2335c779f9c
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-18 10:32:01 +0300

    update log

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,47 +1,22 @@
-import org.apache.commons.lang3.StringUtils;
+import java.util.StringJoiner;
+import org.apache.commons.lang3.ArrayUtils;
 
 class App {
 
-  public static String capitalizeWords(String sentence) {
-    // split разделяет строку по пробелу
-    var words = sentence.split(" ");
-    for (var i = 0; i < words.length; i++) {
-      // Приводим первую букву слова к верхнему регистру
-      // И заменяем слово в массиве
-      words[i] = StringUtils.capitalize(words[i]);
+  public static String makeCensored(String str, String[] censorWords) {
+    var words = str.split(" ");
+    var result = new StringJoiner(" ");
+
+    for (var word : words) {
+      if (ArrayUtils.contains(censorWords, word)) {
+        result.add("$#%!");
+      } else {
+        result.add(word);
+      }
     }
-    // Соединяет массив используя разделитель
-    return String.join(" ", words);
-  }
-
-  public static String buildDefinitionList(String[][] items) {
-
-    if (items.length == 0) {
-      return "";
-    }
-
-    final int titleIndex = 0;
-    final int descrIndex = 1;
-
-    var result = new StringBuilder();
-    result.append("<dl>");
-
-    for (String[] string : items) {
-      result.append("<dt>");
-      result.append(string[titleIndex]);
-      result.append("</dt>");
-
-      result.append("<dd>");
-      result.append(string[descrIndex]);
-      result.append("</dd>");
-    }
-
-    result.append("</dl>");
-
     return result.toString();
   }
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
   }
 }
