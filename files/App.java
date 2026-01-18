e8a89e3773fa4719746bfc2f128e1808e124c395
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-18 08:14:26 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,40 +1,29 @@
-import java.util.Arrays;
-
 class App {
-  public static int getTotalAmount(String[] banknotes, String currency) {
-    String[] tempArr = new String[banknotes.length];
-    int count = 0;
+  public static String getSuperSeriesWinner(int[][] scores) {
+    final int CanadaScoreIndex = 0;
+    final int UssrScoreIndex = 1;
 
-    for (int i = 0; i < banknotes.length; i++) {
-      if (banknotes[i].contains(currency)) {
-        tempArr[count] = banknotes[i].replace(currency, "").trim();
-        count++;
+    int CanadaScore = 0;
+    int UssrScore = 0;
+
+    for (int[] score : scores) {
+      if (score[CanadaScoreIndex] > score[UssrScoreIndex]) {
+        CanadaScore++;
+      } else if (score[CanadaScoreIndex] < score[UssrScoreIndex]) {
+        UssrScore++;
       }
     }
 
-    System.out.println(Arrays.toString(tempArr)); // изменено здесь
-
-    int res = 0;
-    for (String string : tempArr) {
-      if (string == null) {
-        break;
-      }
-      res += Integer.parseInt(string);
+    if (CanadaScore > UssrScore) {
+      return "canada";
+    } else if (CanadaScore < UssrScore) {
+      return "ussr";
+    } else {
+      return "draw";
     }
-
-    System.out.println(res);
-
-    return res;
   }
 
   public static void main(String[] args) {
-    String[] banknotes1 = { "eur 10", "usd 1", "usd 10", "rub 50", "usd 5" };
-    App.getTotalAmount(banknotes1, "usd"); // 16
-
-    String[] banknotes2 = { "eur 10", "usd 1", "eur 5", "rub 100", "eur 20", "eur 100", "rub 200" };
-    App.getTotalAmount(banknotes2, "eur"); // 135
-
-    String[] banknotes3 = { "eur 10", "rub 50", "eur 5", "rub 10", "rub 10", "eur 100", "rub 200" };
-    App.getTotalAmount(banknotes3, "rub"); // 270
+    System.out.println("Hello world");
   }
 }
