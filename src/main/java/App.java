41091d60b3d9217e10995089c859240c646eacde
Kirill Vasilev <kirillwork56@gmail.com>
2026-01-20 21:56:11 +0300

    update log

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,5 @@
-import java.util.StringJoiner;
-import org.apache.commons.lang3.ArrayUtils;
 
 class App {
-
-  public static String makeCensored(String str, String[] censorWords) {
-    var words = str.split(" ");
-    var result = new StringJoiner(" ");
-
-    for (var word : words) {
-      if (ArrayUtils.contains(censorWords, word)) {
-        result.add("$#%!");
-      } else {
-        result.add(word);
-      }
-    }
-    return result.toString();
-  }
 
   public static void main(String[] args) {
   }
