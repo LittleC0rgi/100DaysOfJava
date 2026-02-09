e9d2ea31c732c1f4ea810fd54eac1cd867b10b2c
Kirill Vasilev <kirillwork56@gmail.com>
2026-02-09 23:31:03 +0300

    update log

--- files/App.java
+++ files/App.java
@@ -1,12 +1,7 @@
 class App {
     public static void main(String[] args) {
-        System.out.println(
-                getSquare(10, 10, 60) // Приблизительно 43.3
-        );
-    }
+        for (int i = 0; i < args.length; i++) {
 
-    public static double getSquare(int a, int b, int degrees) {
-        double radians = Math.toRadians(degrees);
-        return (a * b * Math.sin(radians)) / 2;
+        }
     }
 }
